adc2df40d4c9f030ba5cb89616483d3b09a8e39a
htester05 <htester05@19195ebc-ddf4-11de-a25d-e77b6a08f8c3>
2010-01-18 19:46:37 +0000

    Version 1.0: erste lauffähige Version der Anwesenheitsliste

--- VAWIOrgaTool/AusgabeDatenVerwaltung/Datenverwaltung/Anwesenheitsliste.java
+++ VAWIOrgaTool/AusgabeDatenVerwaltung/Datenverwaltung/Anwesenheitsliste.java
@@ -15,7 +15,7 @@ import Hilfsklassen.*;
  * Entit&auml;tstyp f&uuml;r eine Anwesenheitsliste.
  * 
  * @author Martin
- * @version 0.5
+ * @version 1.0
  */
 public class Anwesenheitsliste {
 
@@ -46,28 +46,37 @@ public class Anwesenheitsliste {
 	/**
 	 * Methode zum Erzeugen der Liste
 	 * 
-	 * @return
+	 * @return output-String, der die Ausgabedaten formatiert enthält
 	 */
 	public String generiereListe() {
-
+		
+		//String Variablen, die zum Aufbau der Ausgabestreams
+		//benötigt werden
 		String output_b = new String();
 		String output_d = new String();
 		String output = new String();
+		//int Variable als Counter für die Kurse eines Studenten
+		int count;
 
+		
 		Iterator i_pplan = pruefungsterminplan.getPruefungsplanIterator();
 
+		//loop über die Prüfungstage
 		while (i_pplan.hasNext()) {
 
 			Pruefungstag tmp_pt = (Pruefungstag) i_pplan.next();
 
+			//zu Beginn eines jeden Prüfungstages werden die entsprechenden
+			//Tages-Header in die jeweiligen Output-Strings geschrieben
 			output_b = output_b + "\nBamberg - Tag " + tmp_pt.getTagId()
 					+ "\n\n";
 			output_d = output_d + "\nDuisburg-Essen - Tag " + tmp_pt.getTagId()
 					+ "\n\n";
 
-			Iterator i_studenten = tmp_pt.getTagesStudentenliste()
-					.getStudentIterator();
+			
+			Iterator i_studenten = tmp_pt.getTagesStudentenliste().getStudentIterator();
 
+			//loop über die Studenten am Prüfungstag
 			while (i_studenten.hasNext()) {
 
 				Student tmp_student = (Student) i_studenten.next();
@@ -75,6 +84,8 @@ public class Anwesenheitsliste {
 				Kursliste tageskurse = null;
 				Iterator i_buchungen = null;
 
+				//switch um die Studenten zur richtigen Ausgabe
+				//hinzuzufügen, getrennt nach Universität
 				switch (tmp_student.getUni()) {
 
 				case 'B':
@@ -84,27 +95,35 @@ public class Anwesenheitsliste {
 
 					tageskurse = tmp_pt.getTagesKursliste();
 
-					i_buchungen = buchungsliste.getBuchungen(tmp_student)
-							.getIterator();
+					i_buchungen = buchungsliste.getBuchungen(tmp_student).getIterator();
 
+					//counter auf null
+					count = 0; 
+					
+					//loop über die Buchungen des Studenten
 					while (i_buchungen.hasNext()) {
+												
 						Buchung tmp_buchung = (Buchung) i_buchungen.next();
-
-						if (tageskurse.getKurs(tmp_buchung.getKurs()
-								.getKursid()) != null) {
+						
+						//wenn es der erste Kurs eines Studenten ist und der
+						//aktuelle Kurs in der Tageskursliste vorkommt, dann
+						//ein Komma als Trennzeichen ausgeben
+						if(count != 0 &&
+								tageskurse.getKurs(tmp_buchung.getKurs().getKursid()) != null){
+							output_b = output_b + ", ";
+							}
+						
+						//wenn der aktuelle Kurs in der Tageskursliste vorkommt
+						//dann Ausgabe
+						if (tageskurse.getKurs(tmp_buchung.getKurs().getKursid()) != null) {
 
 							
 							output_b = output_b
-									+ tmp_buchung.getKurs().getKurztitel() + ",";
-
-							
-							
-						}
-
+									+ tmp_buchung.getKurs().getKurztitel(); 
+							count++;	
+						}	
 					}
-					
 					output_b = output_b + "\n";
-
 					break;
 
 				case 'D':
@@ -117,31 +136,39 @@ public class Anwesenheitsliste {
 					i_buchungen = buchungsliste.getBuchungen(tmp_student)
 							.getIterator();
 
+					//counter auf null
+					count = 0; 
+					
+					//loop über die Buchungen des Studenten
 					while (i_buchungen.hasNext()) {
 						Buchung tmp_buchung = (Buchung) i_buchungen.next();
 
+						//wenn es der erste Kurs eines Studenten ist und der
+						//aktuelle Kurs in der Tageskursliste vorkommt, dann
+						//ein Komma als Trennzeichen 
+						if(count != 0 &&
+								tageskurse.getKurs(tmp_buchung.getKurs().getKursid()) != null){
+							output_d = output_d + ", ";
+							}
+						
+						//wenn der aktuelle Kurs in der Tageskursliste vorkommt
+						//dann Ausgabe
 						if (tageskurse.getKurs(tmp_buchung.getKurs()
 								.getKursid()) != null) {
-
 							output_d = output_d
-									+ tmp_buchung.getKurs().getKurztitel() + ",";
-
+									+ tmp_buchung.getKurs().getKurztitel();	
+						count++;
 						}
-
 					}
-					//output_d = output_d.substring(0, output.length());
 					output_d = output_d + "\n";
-
 					break;
-
 				}
-
 			}
-
 		}
 
+		//Gesamt-Output Stream zusammenstellen
 		output = output_b + "\n\n\n" + output_d;
+		
 		return output;
-
 	}
 }
